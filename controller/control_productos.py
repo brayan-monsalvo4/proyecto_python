9a59c7c41e9d157b678688b5cb3d3cb4833f01b1
Brayan Monsalvo <brayanmonsalvo4@gmail.com>
2023-05-06 15:23:49 -0600

    nombre de clase y funciones basicas

---
+++ controller/control_productos.py
@@ -0,0 +1,21 @@
+
+
+class ControlProductos:
+    def __init__(self) -> None:
+        return None
+
+    def obtener_productos(self) -> list:
+        return []
+    
+    def actualizar_producto(self, producto):
+        print("actualizar producto")
+
+    def registrar_producto(self, producto):
+        print("registrar producto")
+
+    def eliminar_producto(self):
+        print("eliminar producto")
+
+    def verificar_datos(self, producto):
+        print("verificar producto")
+        
