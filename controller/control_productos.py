e0d997de44390cfa1a80c37c96114141fb40d214
Brayan Monsalvo <brayanmonsalvo4@gmail.com>
2023-05-08 19:05:45 -0600

    funcion verificar datos

--- controller/control_productos.py
+++ controller/control_productos.py
@@ -1,4 +1,8 @@
+from excepciones.exceptions import *
+from model.producto import Productos as prod
+from model.producto import columnas as columnas_producto
 
+__productos = prod()
 
 class ControlProductos:
     def __init__(self) -> None:
@@ -16,6 +20,32 @@ class ControlProductos:
     def eliminar_producto(self):
         print("eliminar producto")
 
-    def verificar_datos(self, producto):
-        print("verificar producto")
+    def verificar_datos(self, datos_producto):
+        lista_comprobacion = list()
+
+        try:
+            lista_comprobacion = list(map(lambda dato: dato.strip(), datos_producto.values()))
+        except AttributeError:
+            raise DatosIncorrectos
         
+        lista_comprobacion = list( filter(lambda dato: len(dato) != 0, lista_comprobacion) )
+
+        if len(datos_producto) != len(lista_comprobacion):
+            raise CamposVacios
+        
+        for columna, valor in datos_producto.items():
+            try:
+                if columna == "codigo_producto":
+                    int( valor )
+                
+                if columna == "precio":
+                    float( valor )
+                
+                if columna == "cantidad_stock":
+                    int( valor )
+
+                if columna == "duracion_producto":
+                    int( valor )
+
+            except ValueError:
+                raise DatosIncorrectos
